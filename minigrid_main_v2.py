encoder padded step masking bug fix

minigrid_main_v2.py
@@ -28,7 +28,7 @@ from dataclasses import dataclass
 def train_vqvae(model, train_loader, val_loader, num_epochs, learning_rate, device):
 
     # setup wandb logging
-    run_name = f"{args.exp_name}_{args.seed}_{int(time.time())}"
+    run_name = f"{args.exp_name}_{args.seed}_{args.plot_dir}_{int(time.time())}"
     if args.track:
         wandb.init(
             project=args.wandb_project_name,
@@ -360,28 +360,41 @@ def plot_codebook_usage_heatmap(token_ids, agent_labels, n_embeddings, savefile)
     plt.clf()
 
 def plot_tsne(z_qs, agent_labels, savename):
+
+    assert args.n_components in [2, 3], "visualization is only available for n_components = 2 or 3"
+
     z_qs_flattened = np.transpose(z_qs, (0, 2, 1)).reshape(z_qs.shape[0], -1) # flatten 
     
     # reduce dimensionality with PCA
     z_pca = PCA(n_components=100).fit_transform(z_qs_flattened) 
     
     # apply t-SNE
-    z_tsne = TSNE(n_components=2, perplexity=5).fit_transform(z_pca)
+    z_tsne = TSNE(n_components=args.n_components, perplexity=5).fit_transform(z_pca)
 
     unique_agents = sorted(set(agent_labels))
     colors = plt.cm.tab10.colors
     agent_to_color = {agent: colors[i % len(colors)] for i, agent in enumerate(unique_agents)}
 
-    plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(8, 6))
+    if args.n_components == 3:
+        ax = fig.add_subplot(projection="3d")
+
     for agent in unique_agents:
         save_file = f"{savename}_{agent}.png"
         inds = np.where(agent_labels == agent)[0]
         tsne_latents = z_tsne[inds]
-        plt.scatter(
-            tsne_latents[:,0], tsne_latents[:,1], 
-            color=agent_to_color[agent], 
-            label=f"Agent {agent}",
-        )
+        if args.n_components == 2:
+            plt.scatter(
+                tsne_latents[:,0], tsne_latents[:,1], 
+                color=agent_to_color[agent], 
+                label=f"Agent {agent}",
+            )
+        elif args.n_components == 3:
+            ax.scatter(
+                tsne_latents[:,0], tsne_latents[:,1], tsne_latents[:,2],
+                color=agent_to_color[agent],
+                label=f"Agent {agent}",
+            )
         plt.title(f"Trajectory-level z_q Embeddings (Flattened) {agent}")
         plt.legend()
         plt.tight_layout()
@@ -454,7 +467,7 @@ class Args:
     wandb_project_name: str = "human-knowledge-vqvae"
     wandb_entity: str = "ahiranak-university-of-southern-california"
     plot_dir: str = "test"
-    save_interval: int = 20 # number of epochs between each checkpoint saving
+    save_interval: int = 50 # number of epochs between each checkpoint saving
 
     """Dataset Settings"""
     input_seq_len: int = 30 # number of past steps to feed into encoder
@@ -477,9 +490,12 @@ class Args:
     
     """Training Hyperparams"""
     beta: float = 0.25
-    num_epochs: int = 200
+    num_epochs: int = 300
     batch_size: int = 32
-    learning_rate: float = 1e-4    
+    learning_rate: float = 1e-4  
+
+    """Visualization Settings"""
+    n_components: int = 2 # number of components to flatten zq for visualization (2 or 3)  
 
 if __name__ == "__main__":
     args = tyro.cli(Args)

models/encoder.py
@@ -1,6 +1,7 @@
 
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 # import torch.nn.functional as F
 import numpy as np
 import os
@@ -122,14 +123,38 @@ class RNNEncoder(nn.Module):
             bidirectional=bidirectional
         )
 
-    def forward(self, x, lengths=None):
+    # def forward(self, x, lengths=None):
+    #     """
+    #     x: Tensor of shape (B, T, in_dim)
+    #     """
+    #     out, _ = self.rnn(x)
+
+    #     return out  # Shape: (B, T, h_dim * num_directions)
+
+    def forward(self, x, mask, lengths=None):
         """
         x: Tensor of shape (B, T, in_dim)
         """
-        out, _ = self.rnn(x)
+        if mask is not None:
+            lengths = mask.sum(dim=1).cpu()
+        else:
+            lengths = torch.full((x.size(0),), x.size(1), dtype=torch.long, device=x.device)
 
-        return out  # Shape: (B, T, h_dim * num_directions)
+        # Sort by lengths (required by pack_padded_sequence)
+        lengths_sorted, sort_idx = lengths.sort(descending=True)
+        x_sorted = x[sort_idx]
 
+        packed_x = pack_padded_sequence(x_sorted, lengths_sorted, batch_first=True)
+        packed_out, _ = self.rnn(packed_x)
+
+        # pad the output back to the original shape
+        out_sorted, _ = pad_packed_sequence(packed_out, batch_first=True, total_length=x.shape[1]) 
+
+        # Unsort to restore original batch order
+        _, unsort_idx = sort_idx.sort()
+        out = out_sorted[unsort_idx]
+
+        return out
 
 
 if __name__ == "__main__":

models/quantizer.py
@@ -81,13 +81,21 @@ class VectorQuantizer(nn.Module):
         """
         # reshape z -> (batch, height, width, channel) and flatten
         if self.encoder_type == "conv":
+            raise NotImplementedError("Might be outdated")
             # this encoder type outputs z_e with dimension (B, e_dim, C)
             z = z.permute(0, 2, 1).contiguous() # (B, e_dim, C) -> (B, C, e_dim)
             z_flattened = z.reshape(-1, self.e_dim)
         elif self.encoder_type == "trajrnn":
+            raise NotImplementedError("Might be outdated")
             z_flattened = z
         elif self.encoder_type == "timernn":
+            # z_flattened = z.reshape(-1, self.e_dim)
+            # B, T, D = z.shape
             z_flattened = z.reshape(-1, self.e_dim)
+            if mask is not None:
+                flat_mask = mask.reshape(-1).float().unsqueeze(1)  # (B*T, 1)
+            else:
+                flat_mask = None
         else:
             raise NotImplementedError("encoder_type other than conv and rnn are not supported")
         # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
@@ -97,31 +105,61 @@ class VectorQuantizer(nn.Module):
             torch.matmul(z_flattened, self.embedding.weight.t())
 
         # find closest encodings
-        min_encoding_indices = torch.argmin(d, dim=1).unsqueeze(1)
+        min_encoding_indices = torch.argmin(d, dim=1, keepdim=True)
         min_encodings = torch.zeros(
             min_encoding_indices.shape[0], self.n_e).to(z.device)
         min_encodings.scatter_(1, min_encoding_indices, 1)
 
-        # get quantized latent vectors
-        z_q = torch.matmul(min_encodings, self.embedding.weight).view(z.shape)
+        # # get quantized latent vectors
+        # z_q = torch.matmul(min_encodings, self.embedding.weight).view(z.shape)
 
-        # compute loss for embedding (mask out padded timesteps)
-        mask = mask.unsqueeze(-1)
-        commitment_loss = torch.mean(((z_q.detach()-z) * mask) **2)
-        codebook_loss = torch.mean(((z_q - z.detach()) * mask) ** 2)
+        # # compute loss for embedding (mask out padded timesteps)
+        # mask = mask.unsqueeze(-1)
+        # commitment_loss = torch.mean(((z_q.detach()-z) * mask) **2)
+        # codebook_loss = torch.mean(((z_q - z.detach()) * mask) ** 2)
+        # loss = commitment_loss + self.beta * codebook_loss
+
+        # # preserve gradients
+        # z_q = z + (z_q - z).detach()
+
+        # # perplexity
+        # e_mean = torch.mean(min_encodings, dim=0)
+        # perplexity = torch.exp(-torch.sum(e_mean * torch.log(e_mean + 1e-10)))
+
+        # # reshape back to match original input shape
+        # if self.encoder_type == "conv":
+        #     z_q = z_q.permute(0, 2, 1).contiguous()
+        
+        # Quantized latent vector
+        z_q_flat = torch.matmul(min_encodings, self.embedding.weight)  # (B*T, D)
+
+        # Reshape to original shape
+        z_q = z_q_flat.view_as(z)
+
+        # Compute losses — apply masking to both terms
+        if flat_mask is not None:
+            diff = (z_q_flat.detach() - z_flattened) * flat_mask
+            commitment_loss = torch.sum(diff ** 2) / (flat_mask.sum() + 1e-6)
+
+            diff = (z_q_flat - z_flattened.detach()) * flat_mask
+            codebook_loss = torch.sum(diff ** 2) / (flat_mask.sum() + 1e-6)
+        else:
+            # No masking
+            commitment_loss = F.mse_loss(z_q_flat.detach(), z_flattened)
+            codebook_loss = F.mse_loss(z_q_flat, z_flattened.detach())
+
         loss = commitment_loss + self.beta * codebook_loss
 
-        # preserve gradients
+        # Straight-through estimator
         z_q = z + (z_q - z).detach()
 
-        # perplexity
+        # Perplexity
         e_mean = torch.mean(min_encodings, dim=0)
         perplexity = torch.exp(-torch.sum(e_mean * torch.log(e_mean + 1e-10)))
 
-        # reshape back to match original input shape
         if self.encoder_type == "conv":
             z_q = z_q.permute(0, 2, 1).contiguous()
-        
+
         return loss, z_q, perplexity, min_encodings, min_encoding_indices
 
 

models/vqvae.py
@@ -179,8 +179,7 @@ class RNNVQVAE(nn.Module):
         future_mask = x["future_mask"]
 
         # get embedding corresponding to the last non-masked timestep for each trajectory
-        z_e = self.encoder(traj)
-        # breakpoint()
+        z_e = self.encoder(traj, mask)
         # last_real_index = mask.shape[1] - 1 - torch.argmax(torch.flip(mask, dims=[1]), axis=1)
         # z_e = z_e[torch.arange(z_e.shape[0]), last_real_index]
         # pass through VQ layer
@@ -222,7 +221,7 @@ class RNNVQVAE(nn.Module):
         mask = x["mask"]
 
         # get embedding corresponding to the last non-masked timestep for each trajectory
-        z_e = self.encoder(traj)
+        z_e = self.encoder(traj, mask)
 
         # pass through VQ layer
         embedding_loss, z_q, perplexity, min_encodings, min_encoding_indices = self.vector_quantization(z_e, mask)